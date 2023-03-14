a1f82eff97b1d50331d60314762df39d300b0d61
20a91a0578 <84329902+20a91a0578@users.noreply.github.com>
2023-03-14 11:16:46 +0530

    Create Football.sql

---
+++ Football.sql
@@ -0,0 +1,23 @@
+-- create database Football
+-- create table FootballVenue(
+-- venue_id int not null primary key,
+-- venue_name varchar(30) not null,
+-- city_id int not null,
+-- capacity int not null
+-- );
+-- insert into FootballVenue values(2001,'Italy',101,10000);
+-- insert into FootballVenue values(2002,'USA',102,15000);
+-- insert into FootballVenue values(2003,'France',103,12000);
+-- insert into FootballVenue values(2004,'Switzerland',104,10000);
+-- insert into FootballVenue values(2005,'Sweden',105,10000);
+-- insert into FootballVenue values(2006,'West Germany',106,8000);
+-- insert into FootballVenue values(2007,'Mexico',107,10000);
+-- insert into FootballVenue values(2008,'South Africa',108,11000);
+-- insert into FootballVenue values(2009,'Russia',109,18000);
+-- insert into FootballVenue values(2010,'Canada',120,17000);
+
+-- select count(venue_name)  from footballvenue;
+
+-- select  venue_name as Location , capacity as Volume from footballvenue
+
+-- delete from  footballvenue where venue_name='Russia';
