afad07f6dc7978d64797ae22e8462b1503231321
20a91a0578 <84329902+20a91a0578@users.noreply.github.com>
2023-03-14 11:15:22 +0530

    Create Employeedetails.sql

---
+++ Employeedetails.sql
@@ -0,0 +1,28 @@
+-- create database Employee
+-- create table EmployeeDetails (
+-- Emp_id int not null primary key,
+-- Emp_name varchar(30) not null,
+-- Job_name varchar(30) not null,
+-- Manager_id int not null ,
+-- Hire_date varchar(30) not null ,
+-- Salary int not null ,
+-- Dep_id int not null 
+-- );
+
+-- insert into EmployeeDetails values(155,'rajesh','developer',201,'22-jan-2023',55000,1012);
+-- insert into EmployeeDetails values(158,'sai','teamlead',301,'26-feb-2015',70000,1042);
+-- insert into EmployeeDetails values(178,'raj','Tester',501,'02-feb-2018',75000,1085);
+-- insert into EmployeeDetails values(148,'teja','Tester',101,'06-nov-2019',75000,1001);
+-- insert into EmployeeDetails  values(50,'Dharmesh','HR',305,'22-dec-2016',96000,1000);
+-- insert into EmployeeDetails  values(420,'ajay','employee',200,'12-nov-2012',45000,1098);
+
+-- insert into EmployeeDetails  values(555,'arun','employee',212,'12-nov-2012',45000,1045);
+-- insert into EmployeeDetails  values(588,'raaman','employee',234,'16-mar-2019',50000,789);
+-- insert into EmployeeDetails  values(569,'dinesh','employee',857,'19-oct-2020',60000,745);
+-- insert into EmployeeDetails  values(587,'pradeep','Frontend',345,'12-apr-2016',95000,45);
+
+-- select Salary from EmployeeDetails 
+
+-- select distinct Job_name from EmployeeDetails;
+
+-- update EmployeeDetails set Salary= 45000 where Emp_name ="raj";
